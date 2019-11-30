fc7bbb70c3a3cd03b461195aa0be75717dad966a
Anant2019 <55015037+Anant2019@users.noreply.github.com>
2019-11-30 14:19:44 +0530

    Create ass8.cpp

---
+++ ass8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+  char text[200];
+
+  fstream file;
+  file.open ("example.txt", ios::out | ios::in );
+	cout<<"file created example.txt"<<endl;
+
+  cout << "Write text to be written on file." << endl;
+  cin.getline(text, sizeof(text));
+
+  // Writing on file
+  file << text << endl;
+
+  // Reding from file
+  file >> text;
+  cout << text << endl;
+	cout<<"this data stored in file."<<endl;
+
+  //closing the file
+  file.close();
+  return 0;
+}
